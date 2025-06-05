7297d53f3c0b2a19fb865c279da2847f51a53ed1
Belen Yarde Buller <belenyardebuller@gmail.com>
2025-06-05 12:19:57 -0300

    Contenido en README.md + indentacion + ajustes y comentarios en main.py

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# tp-integrador-programacion
-TP Integrador de Programacion / Kaadu - Yarde Buller
+# TP Integrador de Programación: Búsqueda y Ordenamiento
+
+## Integrantes:
+- Arturo Kaadú
+- Belén Yarde Buller
+
+## Imports
+- Usamos librería `requests` para hacer llamadas a la PokéAPI
+- Usamos librería `time` para calcular los tiempos de cada algoritmo de búsqueda
+
+## Funciones
+### get_pokemons()
+Obtiene una lista de los 50 primeros pokemons y guarda su nombre y altura
+Se hacen dos llamados a endpoints:
+- A `/pokemon?offset=0&limit=50` para traer los 50 primeros pokemons.
+- A `https://pokeapi.co/api/v2/pokemon/{pokemon_id}/` para traer la altura de cada pokemon utilizando su id como parámetro en la url.
--- funciones_busqueda.py
+++ funciones_busqueda.py
@@ -1,12 +1,15 @@
 """## Algoritmos de búsqueda"""
 
 def linear_search(pokemons_list, desired_pokemon):
-  print("\n--- Prueba de Búsqueda Lineal: ---")
+    print("\n--- Prueba de Búsqueda Lineal: ---")
 
-  for pokemon in pokemons_list:
-    if pokemon['name'] == desired_pokemon.lower():
-      print(f"'{desired_pokemon}' encontrado: {pokemon}")
-      return pokemon
+    for pokemon in pokemons_list:
+        if pokemon['name'] == desired_pokemon.lower():
+            print(f"'{desired_pokemon}' encontrado: {pokemon}")
+            return pokemon
 
-  print(f"'{desired_pokemon}' no encontrado.")
-  return None
+    print(f"'{desired_pokemon}' no encontrado.")
+    return None
+
+def binary_search(pokemons_list, desired_pokemon):
+    print("\n--- Prueba de Búsqueda Binaria: ---")
